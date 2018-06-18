559582cd51a6357c775a27251f53895d9f69e5a1
Kaivaan <cariaga.info@gmail.com>
2018-06-18 20:50:50 +0800

    test Post

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const history = require('connect-history-api-fallback')
 var serveStatic = require('serve-static')
-
+var request = require('request');
+var bodyParser = require('body-parser');
 const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
 
 
 express()
-  
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .use(staticFileMiddleware)
   .use(history({
     disableDotRule: true,
@@ -22,5 +24,13 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .get('/', (req, res) => res.send('hello'))
   .get('/test', (req, res) =>  res.render(path.join(__dirname + '/dist/index.html')))
-  
+  .post('/register', (req, res) => { 
+
+    var query1=request.body.var1;
+    var query2=request.body.var2;
+    
+    console.log(var1+" "+var2);
+  })
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
